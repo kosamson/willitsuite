76d63a5d0a9440dad866be3e69e019ddba21d011
kosamson <kosamson1@gmail.com>
2024-12-15 01:00:42 -0800

    connect to google over tcp

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"log/slog"
+	"net"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	conn, err := net.Dial("tcp", "google.com:80")
+	if err != nil {
+		slog.Error("failed to connect",
+			"error", err)
+	}
+
+	defer conn.Close()
+
+	slog.Info("successfully connected")
 }
